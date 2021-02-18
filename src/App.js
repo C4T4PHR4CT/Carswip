0cae72cfc43bf86120bfb98cb53a527b7355f166
C4T4PHR4CT <56405586+C4T4PHR4CT@users.noreply.github.com>
2021-02-18 15:50:30 +0100

    ErrorBoundary

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    this.setState(({counter}) => ({
+      counter: counter + 1
+    }));
+  }
+  
+  render() {
+    if (this.state.counter === 5) {
+      throw new Error('Crashed.');
+    }
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
+  }
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: "" };
+  }
+  componentDidCatch(error, info) {
+    this.setState({ error: "Error: " + error.message });
+  }
+  render() {
+    if (this.state.error != "")
+      return <p style={{color: 'red'}}>{this.state.error}</p>;
+    return this.props.children;
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <p>
+        <b>
+          Click on the numbers to increase the counters.
+          <br />
+          The counter is programmed to throw an error when it reaches 5. This simulates a JavaScript error in a component.
+        </b>
+      </p>
+      <hr />
+        <ErrorBoundary>
+            <Counter />
+        </ErrorBoundary>
+        <ErrorBoundary>
+            <Counter />
+        </ErrorBoundary>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
